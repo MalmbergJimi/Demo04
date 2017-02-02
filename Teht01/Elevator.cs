c568b718bdd2d930aec7fe6525e687091230939f
K8718 Malmberg Jimi <K8718@student.jamk.fi>
2017-02-02 14:42:17 +0200

    Hissi toimii kerran.

---
+++ Teht01/Elevator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Elevator
+    {
+        private int MaxKerros = 5;
+        private int floor;
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value <= MaxKerros && value > 0) floor = value;
+                else
+                {
+                    Console.WriteLine("Ei mahdollinen kerros! Rakennuksessa on vain viisi kerrosta!");
+                    floor = Floor;
+                }
+            }
+        }
+    }
+}
+
---
+++ Teht01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator hissi = new Elevator();
+            hissi.Floor = 1;
+            
+            
+            Console.WriteLine("Olet kerroksessa " + hissi.Floor + ". Mihin kerrokseen haluat mennä? ");
+            string line = Console.ReadLine();
+            hissi.Floor = int.Parse(line);
+
+            Console.WriteLine("Olet kerroksessa " + hissi.Floor);
+
+        }
+    }
+}
