f7ff97161ae225903b97cf2e0d3bfdd4e2846e93
K8718 Malmberg Jimi <K8718@student.jamk.fi>
2017-02-02 14:18:07 +0200

    Badum tsih

--- PersonApplication/Program.cs
+++ PersonApplication/Program.cs
@@ -10,6 +10,24 @@ namespace PersonApplication
     {
         static void Main(string[] args)
         {
+            // Create one teacher from Teacher class
+            Teacher ope = new Teacher();
+            ope.FirstName = "Hissi";
+            ope.LastName = "Hilleri";
+            ope.Address = "Valtukunnankatu 15";
+            ope.Age = 55;
+            ope.PhoneNumber = "010-5557771";
+            ope.Room = "229";
+            Console.WriteLine(ope.ToString());
+
+            // Create one Student from Student class
+            Student oppilas = new Student("Kalle", "Kakkahousu", "K4KK4");
+            oppilas.Address = "Housunkuja 2";
+            oppilas.Age = 3;
+            oppilas.PhoneNumber = "444-44444444";
+            Console.WriteLine(oppilas.ToString());
+
+
         }
     }
 }
