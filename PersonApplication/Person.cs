7ac481175bb68710ad4141e9f671af33f6789a62
K8718 Malmberg Jimi <K8718@student.jamk.fi>
2017-02-02 14:08:34 +0200

    Tuntiesimerkki

---
+++ PersonApplication/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Person
+    {
+        // properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+
+        //  default constructor
+        public Person()
+        {
+        }
+        
+        public Person(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        // methods
+
+        public void PersonMethod()
+        {
+            Console.WriteLine("This method belongs to Person man!");
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + Address + " " + Age + " " + PhoneNumber;
+        }
+
+
+    }
+}
---
+++ PersonApplication/Teacher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Teacher : Person
+    {
+        public string Room { get; set; }
+
+        public Teacher()
+        {
+        }
+
+        public Teacher(string firstname, string lastname, string room)
+            : base(firstname, lastname)
+        {
+            Room = room;
+        }
+
+        public void TeacherMethod()
+        {
+            Console.WriteLine("This method belongs to the TEACHEER!");
+        }
+
+        public override string ToString()
+        {
+            // Base on tässä tapauksessa "Person"-luokka
+            // Eli kutsutaan Person-luokan ToString()-metodia
+            // return FirstName + " " + LastName + " " ... + Room;
+            return base.ToString() + " " + Room;
+        }
+
+    }
+}
