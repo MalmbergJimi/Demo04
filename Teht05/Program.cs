015828c462503cc9bc405b5d8236333d2200abf7
K8718 Malmberg Jimi <K8718@student.jamk.fi>
2017-02-09 10:43:34 +0200

    Radio kunnossa, mutta ei vaadi radiota käyntiin.

---
+++ Teht05/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Luodaan Radio ratio
+            Radio ratio = new Radio();
+            ratio.IsOn = true;
+            ratio.Volume = 1;
+            ratio.Taajuus = 1500.0;
+
+            ratio.PrintData();
+
+            //Luodaan Radio aalto
+            Radio aalto = new Radio();
+            aalto.IsOn = true;
+            aalto.Volume = 55;
+            aalto.Taajuus = 25000.5;
+
+            aalto.PrintData();
+
+            //Luodaan Radio puhe
+            Radio puhe = new Radio();
+            puhe.IsOn = true;
+            puhe.Volume = 89;
+            puhe.Taajuus = 1900.0;
+
+            puhe.PrintData();
+
+            //Luodaan Radio puhe
+            Radio poop = new Radio();
+            poop.IsOn = true;
+            poop.Volume = -5;
+            poop.Taajuus = 27000.0;
+
+            poop.PrintData();
+
+
+
+            /*
+            do
+            {
+                Console.WriteLine("Anna Radion äänenvoimakkuus (0-100): ");
+                string line = Console.ReadLine();
+                ratio.Volume = int.Parse(line);
+
+                Console.WriteLine("Äänenvoimakkuus on nyt: " + ratio.Volume);
+                Console.WriteLine();
+                i++;
+            } while (i < 5); */
+        }
+    }
+}
---
+++ Teht05/Radio.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht05
+{
+    class Radio
+    {
+        private int maxvolume = 100;
+        private int minvolume = 0;
+        private double maxtaajuus = 26000.0;
+        private double mintaajuus = 2000.0;
+        private int volume;
+        private double taajuus;
+        public bool IsOn { get; set; }
+            
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxvolume && value >= minvolume) volume = value;
+                else if (value <= minvolume)
+                {
+                    Volume = 0;
+                    volume = Volume;
+                    Console.WriteLine("** Ei mahdollinen arvo! Aseta arvo välille 0-100. **");
+                }
+                else
+                {
+                    Volume = 100;
+                    volume = Volume;
+                    Console.WriteLine("** Ei mahdollinen arvo! Aseta arvo välille 0-100. **");
+
+                }
+            }
+        }
+        public double Taajuus
+        {
+            get
+            {
+                return taajuus;
+            }
+            set
+            {
+                if (value <= maxtaajuus && value >= mintaajuus) taajuus = value;
+                else if (value <= mintaajuus)
+                {
+                    Taajuus = 2000.0;
+                    taajuus = Taajuus;
+                    Console.WriteLine("** Valittu taajuus liian pieni. Asetettu taajuudelle 2000.0 **");
+                    Console.WriteLine("** Taajuudet valittavissa väliltä 2000.0 - 26000.0 **");
+                    
+                }
+                else
+                {
+                    Taajuus = 26000.0;
+                    taajuus = Taajuus;
+                    Console.WriteLine("** Valittu taajuus liian suuri. Asetettu taajuudelle 26000.0 **");
+                    Console.WriteLine("** Taajuudet valittavissa väliltä 2000.0 - 26000.0 **");
+                    
+                }
+            }
+
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Onko Radio päällä?: " + IsOn);
+            Console.WriteLine("Äänenvoimakkuus on: " + Volume);
+            Console.WriteLine("Taajuus on: " + Taajuus);
+            Console.WriteLine();
+        }
+    }
+}
